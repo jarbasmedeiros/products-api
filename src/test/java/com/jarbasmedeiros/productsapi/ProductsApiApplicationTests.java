c47281261cf2cbd06f9e923840692cdd4460d4b1
Jarbas Medeiros <jarbasmedeirosfilho@gmail.com>
2021-03-07 12:23:15 -0300

    Project created

---
+++ src/test/java/com/jarbasmedeiros/productsapi/ProductsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarbasmedeiros.productsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
