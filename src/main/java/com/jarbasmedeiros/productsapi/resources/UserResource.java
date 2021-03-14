efb4b662af7386b3ce21fa9659e1ea01db72a67a
Jarbas Medeiros <jarbasmedeirosfilho@gmail.com>
2021-03-14 07:14:59 -0300

    Service layer, component registration

--- src/main/java/com/jarbasmedeiros/productsapi/resources/UserResource.java
+++ src/main/java/com/jarbasmedeiros/productsapi/resources/UserResource.java
@@ -1,20 +1,34 @@
 package com.jarbasmedeiros.productsapi.resources;
 
 import com.jarbasmedeiros.productsapi.entities.User;
+import com.jarbasmedeiros.productsapi.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
-    @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L,"Jarbas","jarbas@gmail.com","999999999","");
+    @Autowired
+    private UserService service;
 
-        return ResponseEntity.ok().body(u);
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
     }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
 
 }
